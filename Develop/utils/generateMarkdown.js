f26c1735fc3febe26c16d6607a9d762bd9e6ac32
Atul Mahajan <luta_atul@hotmail.com>
2020-04-04 01:31:19 +1100

    Adding contents into the readme.md through generateMarkdown.

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,19 @@
 function generateMarkdown(data) {
   return `
-# ${data.title}
+  # Project Title: ${data[0].title}\n
+  # Profile Image\n
+  ![Screenshot](${data[1].data.avatar_url})\n
+  # Email Address\n
+  ${data[1].data.email}\n
+  Table of Contents:\n
+  [Description](#Description:)\n
+  [Installation](#Installation:)\n
+  [Usage](#Usage:)\n
+  [Licence](#Licene:)\n
+  [Contributing](#Contributing:)\n
+  [Tests](#Tests:)\n
+  [Questions](#Questions:)\n
+
 
 `;
 }
