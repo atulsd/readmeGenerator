700079abaed849e4bcddbbc5b7e89dc6ad65a812
Atul Mahajan <luta_atul@hotmail.com>
2020-04-03 22:25:13 +1100

    Getting gitHub user name to retrieve information.

--- Develop/utils/api.js
+++ Develop/utils/api.js
@@ -1,6 +1,21 @@
+const axios = require("axios");
+const inquirer = require("inquirer");
+
 const api = {
   getUser(username) {
-
+    inquirer
+      .prompt({
+        type: "input",
+        message: "Enter your GitHub username",
+        name: "username"
+      })
+      .then(function({ username }) {
+        const queryUrl = `https://api.github.com/users/${username}`;
+        axios.get(queryUrl).then(function(res) {
+          console.log(res.data.avatar_url);
+          console.log(res.data.email);
+        });
+      });
   }
 };
 
