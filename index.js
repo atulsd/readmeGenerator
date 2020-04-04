f9b3af859590b5341740c657a14458146b703cc1
Atul Mahajan <luta_atul@hotmail.com>
2020-04-04 17:04:39 +1100

    Cleaned and indented file.

--- index.js
+++ index.js
@@ -1,54 +1,36 @@
-const api = require("./utils/api");
-const generateMarkdown = require("./utils/generateMarkdown");
+const api = require("./Develop/utils/api");
+const generateMarkdown = require("./Develop/utils/generateMarkdown");
 const inquirer = require("inquirer");
 
 const fs = require("fs");
 const util = require("util");
 const writeFileAsync = util.promisify(fs.writeFile);
 
-const questions = [
-  "Enter your GitHub username:",
-  "Enter your Project title:",
-  "Enter your Project description:",
-  "Enter name of the repo:"
-];
-
-function writeToFile(fileName, ...data) {
-  const contents = generateMarkdown(data);
-  writeFileAsync(fileName, contents).then(function() {
-    console.log("Successfully wrote to Readme.md file");
-  });
-}
-
-let user;
-let enteredDetails;
 async function init() {
   try {
-    await inquirer
-      .prompt([
-        {
-          message: questions[0],
-          name: "username"
-        },
-        {
-          message: questions[1],
-          name: "title"
-        },
-        {
-          message: questions[2],
-          name: "desc"
-        },
-        {
-          message: questions[3],
-          name: "repo"
-        }
-      ])
-      .then(function(response) {
-        enteredDetails = response;
-        user = response.username;
-      });
-    await api.getUser(user);
-    await writeToFile("Readme.md", enteredDetails, api.data, questions);
+    const { username, title, desc, repo } = await inquirer.prompt([
+      {
+        message: "Enter your GitHub username:",
+        name: "username",
+      },
+      {
+        message: "Enter your Project title:",
+        name: "title",
+      },
+      {
+        message: "Enter your Project description:",
+        name: "desc",
+      },
+      {
+        message: "Enter name of the repo:",
+        name: "repo",
+      },
+    ]);
+    const { data } = await api.getUser(username);
+    console.log(data);
+    const contents = generateMarkdown(username, title, desc, repo, data);
+    await writeFileAsync("Readme.md", contents);
+    console.log("Successfully wrote to Readme.md file");
   } catch (err) {
     console.log("Can not read properties.");
   }
