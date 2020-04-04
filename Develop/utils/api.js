058c50f206811d3e31435f43274660e3de071de6
Atul Mahajan <luta_atul@hotmail.com>
2020-04-04 22:03:25 +1100

    Added dotenv for client details.

--- Develop/utils/api.js
+++ Develop/utils/api.js
@@ -1,14 +1,16 @@
 const axios = require("axios");
+require("dotenv").config();
 
-const api = {
-  async getUser(username) {
-    try {
-      const queryUrl = `https://api.github.com/users/${username}`;
-      return await axios.get(queryUrl);
-    } catch (err) {
-      console.log("Please check your details.");
-    }
-  },
+const clientId = process.env.ClientId;
+const clientSecret = process.env.ClientSecret;
+
+const getUser = async (username) => {
+  try {
+    const queryUrl = `https://api.github.com/users/${username}?client_id=${clientId}&client_secret=${clientSecret}`;
+    return await axios.get(queryUrl);
+  } catch (err) {
+    console.log("Please check your details.");
+  }
 };
 
-module.exports = api;
+module.exports = { getUser };
