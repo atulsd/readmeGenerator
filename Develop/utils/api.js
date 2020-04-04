a6f7c82f35b8a66a224f7bd52921c4055f5c0807
Atul Mahajan <luta_atul@hotmail.com>
2020-04-04 17:05:52 +1100

    Cleaned and indented file.

--- Develop/utils/api.js
+++ Develop/utils/api.js
@@ -1,17 +1,14 @@
 const axios = require("axios");
 
 const api = {
-  data: "",
   async getUser(username) {
     try {
       const queryUrl = `https://api.github.com/users/${username}`;
-      await axios.get(queryUrl).then(function(res) {
-        api.data = res;
-      });
+      return await axios.get(queryUrl);
     } catch (err) {
       console.log("Please check your details.");
     }
-  }
+  },
 };
 
 module.exports = api;
